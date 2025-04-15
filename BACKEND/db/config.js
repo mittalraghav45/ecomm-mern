aa536e0323ff51990093a498a7ba166f27cf7323
Raghav Mittal <mittalraghav45@gmail.com>
2025-04-15 19:29:48 +0100

    delete operation

---
+++ BACKEND/db/config.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/e-commerce", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to MongoDB:', err);
+});
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+
+require("./BACKEND/db/config");
+const User = require("./BACKEND/db/User");
+const Product = require('./BACKEND/db/Product')
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Route to handle user registration
+app.post("/register", async (req, res) => {
+  try {
+    let user = new User(req.body);
+    let result = await user.save();
+    res.status(201).send({ result });
+    
+  } catch (err) {
+    res
+      .status(500)
+      .send({ error: "Registration failed", message: err.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  let user = await User.findOne(req.body).select("-password");
+  if (req.body.email && req.body.password) {
+    if (user) {
+      res.status(200).json({ user });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  }
+});
+
+app.post("/add-product",async (req,res)=>{
+  let product = new Product(req.body);
+  let result = await product.save();
+  res.send(result)
+})
+// Start the server on port 5000
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
