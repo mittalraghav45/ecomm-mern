f607495107664fb305a4927487499c5c2da62d76
Raghav Mittal <mittalraghav45@gmail.com>
2025-04-16 01:24:55 +0100

    update api works on postman

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 require("./BACKEND/db/config");
 const User = require("./BACKEND/db/User");
-const Product = require('./BACKEND/db/Product')
+const Product = require("./BACKEND/db/Product");
 const app = express();
 
 app.use(express.json());
@@ -15,7 +15,6 @@ app.post("/register", async (req, res) => {
     let user = new User(req.body);
     let result = await user.save();
     res.status(201).send({ result });
-    
   } catch (err) {
     res
       .status(500)
@@ -34,26 +33,46 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.post("/add-product",async (req,res)=>{
+app.post("/add-product", async (req, res) => {
   let product = new Product(req.body);
   let result = await product.save();
-  res.send(result)
-})
+  res.send(result);
+});
 
-app.get("/products",async(req,resp)=>{
-  let products= await Product.find();
-  if(products.length>0){
+app.get("/products", async (req, resp) => {
+  let products = await Product.find();
+  if (products.length > 0) {
     resp.send(products);
-    }else{
-      resp.send({result:"No products Found"})
+  } else {
+    resp.send({ result: "No products Found" });
   }
-})
+});
 
-app.delete("/product/:id",async (req,resp)=>{
-//  resp.send('working',req.params.id);
- const result = await Product.deleteOne({_id:req.params.id});
- resp.send(result);
-})
+app.delete("/product/:id", async (req, resp) => {
+  //  resp.send('working',req.params.id);
+  const result = await Product.deleteOne({ _id: req.params.id });
+  resp.send(result);
+});
+
+app.get("/product/:id", async (req, res) => {
+  let result = await Product.findOne({ _id: req.params.id });
+  if (result) {
+    res.send(result);
+  } else {
+    res.send({ result: "NO result found" });
+  }
+});
+
+app.put("/product/:id", async (req, res) => {
+  let result = await Product.updateOne(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  );
+  res.send(result);
+});
+
 // Start the server on port 5000
 app.listen(5000, () => {
   console.log("Server running on port 5000");
